5da0d0502a819dd206a856267778157bba0528cb
Monocled004 <145308707+Monocled004@users.noreply.github.com>
2025-01-12 11:35:33 +0530

    Delete stumpwm directory

--- stumpwm/remove.lisp
+++
@@ -1,8 +0,0 @@
-
-(defun tiling-remove (window-id)
-  (let ((frame (window-frame window-id)))
-    (if (not (= 0 (length (screen-frames (current-screen)))))
-	(if (= 1 1)
-	    (remove-split)))))
-
-(stumpwm:add-hook *destroy-window-hook* 'tiling-remove)
