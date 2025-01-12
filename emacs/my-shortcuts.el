cf1174673ece2a6d88546422c4587041c616761e
Monocled004 <145308707+Monocled004@users.noreply.github.com>
2025-01-12 11:33:17 +0530

    Delete emacs directory

--- emacs/my-shortcuts.el
+++
@@ -1,9 +0,0 @@
-
-;;; Buffer swapping
-;;; Code:
-(global-set-key (kbd "C-c <up>")     'windmove-up)
-(global-set-key (kbd "C-c <down>")   'windmove-down)
-(global-set-key (kbd "C-c <left>")   'windmove-left)
-(global-set-key (kbd "C-c <right>")  'windmove-right)
-(provide 'myshortcuts)
-;;; myshortcuts.el ends here
